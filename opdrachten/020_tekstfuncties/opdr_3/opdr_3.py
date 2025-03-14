094b465ff4e69c047d6dfa69280c4b3a34fdacb5
iYukiNL <bertverhoeff7-7-1990@hotmail.com>
2025-03-14 13:59:18 +0100

    tree

--- opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
+++ opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
@@ -1,5 +1,16 @@
 # Opdracht 3 tekstfuncties
-# Naam student:
+# Naam student:Bert
 # Groep:
 
 # Hier komt je code...
+boomrij= ("           *             ")
+boomrij1=("          ***            ")
+boomrij2=("         ******          ")
+boomrij3=("        ********         ")
+boomrij4=("       ***********       ")
+boomrij5=("           ***           ")
+boomrij6=("           ***           ")
+boomrij7=("           ***           ")
+tree =(f"{boomrij * 5}\n{boomrij1 * 5}\n{boomrij2 * 5}\n{boomrij3 * 5}\n{boomrij4 * 5}\n{boomrij5 * 5}\n{boomrij6 * 5}\n{boomrij7 * 5}")
+
+print (tree)
