5cf9acb3f77bba309bb771dd59ca76c575f86841
iYukiNL <bertverhoeff7-7-1990@hotmail.com>
2025-03-17 10:49:46 +0100

    aantalBomen code

--- opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
+++ opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
@@ -3,6 +3,7 @@
 # Groep:
 
 # Hier komt je code...
+aantalBomen = 4
 boomrij= ("           *             ")
 boomrij1=("          ***            ")
 boomrij2=("         ******          ")
@@ -11,6 +12,6 @@ boomrij4=("       ***********       ")
 boomrij5=("           ***           ")
 boomrij6=("           ***           ")
 boomrij7=("           ***           ")
-tree =(f"{boomrij * 5}\n{boomrij1 * 5}\n{boomrij2 * 5}\n{boomrij3 * 5}\n{boomrij4 * 5}\n{boomrij5 * 5}\n{boomrij6 * 5}\n{boomrij7 * 5}")
+tree =(f"{boomrij * aantalBomen}\n{boomrij1 * aantalBomen}\n{boomrij2 * aantalBomen}\n{boomrij3 * aantalBomen}\n{boomrij4 * aantalBomen}\n{boomrij5 * aantalBomen}\n{boomrij6 * aantalBomen}\n{boomrij7 * aantalBomen}")
 
 print (tree)
