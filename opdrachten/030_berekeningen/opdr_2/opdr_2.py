cf3091aaa7bda3889f885e89dd03b8abbefa7fa4
iYukiNL <bertverhoeff7-7-1990@hotmail.com>
2025-03-17 12:33:19 +0100

    Fahrenheit en Celsius

--- opdrachten/030_berekeningen/opdr_2/opdr_2.py
+++ opdrachten/030_berekeningen/opdr_2/opdr_2.py
@@ -4,7 +4,25 @@
 
 # Hier komt je code...
 
-c = ...
-f = ...
+c = -12
+fahrenheit = (c * 1.8) + 32
+print('%.2f Celsius is equivalent to: %.2f Fahrenheit'
+      % (c, fahrenheit))
+#-12 graden Celsius is gelijk aan 10.4 graden Fahrenheit  
+c = 62.2
+fahrenheit = (c * 1.8) + 32
+print('%.2f Celsius is equivalent to: %.2f Fahrenheit'
+      % (c, fahrenheit))
+#62.2 graden Celsius is gelijk aan 144 graden Fahrenheit  
 
-print()
+f = 96
+celsius = (f-32)/1.8
+print('%.2f Fahrenheit is equivalent to: %.2f Celsius'
+      % (f ,celsius))
+# 96 graden Fahrenheit is gelijk aan 35.6 graden Celcius
+f = 102
+celsius = (f-32)/1.8
+print('%.2f Fahrenheit is equivalent to: %.2f Celsius'
+      % (f ,celsius))
+# 102 graden Fahrenheit is gelijk aan 38.9 graden Celcius
+
