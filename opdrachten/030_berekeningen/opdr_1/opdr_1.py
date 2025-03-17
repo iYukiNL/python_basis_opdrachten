2d0a19299cda914d535b8f78cfa39f11df4aa29c
iYukiNL <bertverhoeff7-7-1990@hotmail.com>
2025-03-17 11:03:49 +0100

    Berekeningen.

--- opdrachten/030_berekeningen/opdr_1/opdr_1.py
+++ opdrachten/030_berekeningen/opdr_1/opdr_1.py
@@ -1,5 +1,12 @@
 # Opdracht 1 berekeningen
-# Naam student:
+# Naam student:Bert 
 # Groep:
 
 # Hier komt je code...
+#naam + Leeftijd van de jongens.
+Joe = 20
+Fred = 20
+Bob = 20
+aantalNamen = 3
+print (Joe + Fred + Bob)
+print ((Joe + Fred + Bob) /aantalNamen )
